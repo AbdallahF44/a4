3bb06a59caff3202f4adee555e9182c704ae9039
Abdallah Fawzi <119876102+AbdallahF44@users.noreply.github.com>
2023-04-18 23:14:27 +0300

    sql

--- a4.sql
+++ a4.sql
@@ -1,7 +1,7 @@
 -- phpMyAdmin SQL Dump
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
---
+-- 
 -- Host: 127.0.0.1
 -- Generation Time: Apr 06, 2023 at 02:31 PM
 -- Server version: 10.4.27-MariaDB
